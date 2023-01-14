f0a0b3d8bb8625df40482eecdf1079f988fa5e5c
PointZero4 <66811575+PointZero4@users.noreply.github.com>
2023-01-14 05:05:15 +0000

    Update 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # SSH client configuration
-exec { 'echo -e "IdentityFile ~/.ssh/holberton\n" >> /etc/ssh/ssh_config':
+exec { 'echo -e "IdentityFile ~/.ssh/school\n" >> /etc/ssh/ssh_config':
   provider => shell,
   path    => '/etc/ssh/ssh_config',
   command => 'echo -e "PasswordAuthentication no\n" >> /etc/ssh/ssh_config'
